8f6adef61196a28ab2e98f5360099871f5c6946f
nuggy875 <jinugpaeng@gmail.com>
2018-03-31 22:59:24 +0900

    code refactoring

--- python_server/python-code/test.py
+++ python_server/python-code/test.py
@@ -1,8 +1,6 @@
-import cv2
 import sys
 
-img_input = cv2.imread('dog.png')
-print('hello world')
-print(img_input)
-print('img is sent')
+print('input image')
+print('img Preprocessing process')
+print('return matching score')
 print(sys.argv)
