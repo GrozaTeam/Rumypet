6aeac65af728ea4749169a264a3d9fcc257ef9d4
nuggy875 <jinugpaeng@gmail.com>
2018-04-01 19:23:54 +0900

    merge

--- python_server/server/routes/index.js
+++ python_server/server/routes/index.js
@@ -4,23 +4,19 @@ var PythonShell = require('python-shell');
 var resultPython;
 
 var options = {
-
   mode: 'text',
   pythonPath: '',
   pythonOptions: ['-u'],
   scriptPath: './python-code',
   args: ['value1', 'value2', 'value3']
 };
-
 router.get('/', function(req,res){
   res.render('start',{
     title: 'global cau',
     user: req.user
   });
 });
-
 router.get('/python', function(req, res) {
-
   PythonShell.run('test.py', options, function (err, resultPython) {
 
     if (err) throw err;
