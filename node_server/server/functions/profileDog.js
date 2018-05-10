eb73de714ed51bda82cf5be267499b3254b8b3ef
nuggy875 <jinugpaeng@gmail.com>
2018-05-10 16:31:57 +0900

    code refactoring

--- node_server/server/functions/profileDog.js
+++ node_server/server/functions/profileDog.js
@@ -2,12 +2,15 @@ var dog = require('../models/dog');
 
 exports.getDogProfile = email =>
 
-	new Promise((resolve,reject) => {
+	new Promise(function(resolve,reject){
 
 		dog.find({ ownerId: email })
 
-		.then(dogs => resolve(dogs))
-
-		.catch(err => reject({ status: 500, message: 'Internal Server Error !' }))
+		.then(function(dogs){
+			resolve(dogs);
+		})
+		.catch(function(err){
+			reject({ status: 500, message: 'Internal Server Error !' });
+		});
 
 	});
